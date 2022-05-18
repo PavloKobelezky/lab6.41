22e48b215a12beb58c74f56fe8a8dd9c68d1604a
Кобилецький Павло <102289447+PavloKobelezky@users.noreply.github.com>
2022-05-18 11:45:20 +0300

    Add files via upload

---
+++ lab6.41/src/com/company/Colors.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Colors {
+    int Colors;
+
+    void colorMotorcycle(String Color) {
+
+        if (Color == "Red") {
+            System.out.println("Червоного кольору");
+
+        } else if (Color == "Blue") {
+            System.out.println("Синього кольору");
+
+        } else if (Color == "Blak") {
+            System.out.println("Чорного кольору");
+
+        } else if (Color == "Yellow") {
+            System.out.println("Жовтого кольору");
+
+        } else if (Color == "Green") {
+            System.out.println("Зеленого кольору");
+
+        }else {
+            System.out.println("Немає такого кольору");
+        }
+
+    }
+
+}
+
+
---
+++ lab6.41/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+        public static void main(String[] args) {
+
+            Motorcycle motorcycles = new Motorcycle();
+            Colors colors = new Colors();
+            Stage stage = new Stage();
+
+            motorcycles.buyModels("XROAD202CBF");
+            motorcycles.Totalmotorcycle();
+            colors.colorMotorcycle("Yellow");
+            stage.buyStage("two");
+            stage.totalstage();
+
+
+
+
+        }
+
+
+    }
+
+
+
